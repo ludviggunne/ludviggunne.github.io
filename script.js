2749ca075eb7905071abee3867ba0b98a6f6aed3
Ludvig Gunne Lindström <ludd.lindstroem@gmail.com>
2023-04-13 20:29:11 +0200

    cool scrolling color effect

--- script.js
+++ script.js
@@ -23,3 +23,23 @@ createEntry('qualia', 'visualizing quaternion julia sets using compute shaders')
 createEntry('raytracer', 'simple raytracer written in C');
 createEntry('iksolver', 'stb-style single header library for inverse kinematics using FABRIK model');
 createEntry('ludviggunne.github.io', 'this website');
+
+
+window.addEventListener('scroll', function() {
+    let scrollPos = window.scrollY;
+    let body = document.querySelector('body');
+    let s = scrollPos / (document.documentElement.scrollHeight - window.innerHeight);
+
+    let r0 = 35;
+    let g0 = 49;
+    let b0 = 49;
+    let r1 = 49;
+    let g1 = 33;
+    let b1 = 45;
+
+    let r = r0 + s * (r1 - r0);
+    let g = g0 + s * (g1 - g0);
+    let b = b0 + s * (b1 - b0);
+
+    body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
